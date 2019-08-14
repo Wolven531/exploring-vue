70b3144b19cdc26e1f6403ba57bfeef1446b903d
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-13 21:39:19 -0600

    Use Resources component in App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<HelloWorld msg="Welcome" />
+		<Resources money="100" />
 	</div>
 </template>
 <script>
 	import HelloWorld from './components/HelloWorld.vue'
+	import Resources from './components/Resources.vue'
 
 	export default {
 		name: 'app',
 		components: {
-			HelloWorld
+			HelloWorld,
+			Resources
 		}
 	}
 </script>
