889f3d5dd9110bad29144c282eaad09f427f6b79
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-13 21:37:49 -0600

    Introduce Resources component

---
+++ src/components/Resources.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="resources">
+		<h2>Resources</h2>
+		<p>Money: {{money}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Resources',
+		props: {
+			money: Number
+		}
+	}
+</script>
